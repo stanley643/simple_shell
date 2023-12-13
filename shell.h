de78b59effef6138718af2dc1f2f16479b788642
Stanley Murigi Njoroge <98167574+stanley643@users.noreply.github.com>
2023-12-13 21:46:24 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -13,12 +13,13 @@
 #define BUFSIZE 1024
 #define MAX_ARGS 11
 
-char *create_buf(char *buf);
+char *create_buffer(char *buf);
 void _print_str(char *c);
 int _putchar(char c);
 void tokenize_args(char **args_array, char *str);
-char *find_the_path(char *command, char *path[]);
+char *find_path(char *command, char *path[]);
 void remove_newline(char *str);
 void print_env(char **env);
+void init_path(char *path_var, char **path);
 
 #endif
