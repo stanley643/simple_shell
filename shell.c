7616f9f8434137cbef31c227a14b96266af81b60
stanley643 <nmurigi@kabarak.ac.ke>
2023-12-13 06:30:37 -0800

    code a code

--- shell.c
+++ shell.c
@@ -8,4 +8,27 @@
 
 int main(int ac, char **av)
 {
-	
+char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    (void)ac;
+    (void)av;
+
+    while (1) {
+        printf(" ");
+
+        nread = getline(&line, &len, stdin);
+
+        if (nread == -1) {
+            printf("\nEnd of input. Exiting.\n");
+            break;
+        }
+
+        printf("%s", line);
+    }
+
+    free(line);
+
+    return 0;
+}
