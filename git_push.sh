9bac7fb52216647fa332d502a5dabd0114170ff5
stanley643 <nmurigi@kabarak.ac.ke>
2023-12-13 06:18:58 -0800

    code a code

---
+++ git_push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Display a message indicating the script has started
+echo "Starting Git Add, Commit, and Push"
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a default commit message
+git commit -m "code a code"
+
+# Push the changes to the remote repository (assuming origin and master branch, modify if needed)
+git push origin master
+
+# Display a message indicating the script has completed
+echo "Git Add, Commit, and Push completed"
+
---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * main: the playground for the shell
+ * return: return 0;
+ *
+ */
+
+int main(int ac, char **av)
+{
+	
