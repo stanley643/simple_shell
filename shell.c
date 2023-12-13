5d16748b01e1ce68fc9cade07dd23d809a54c2ae
stanley643 <nmurigi@kabarak.ac.ke>
2023-12-13 07:20:15 -0800

    code a code

--- shell.c
+++ shell.c
@@ -6,65 +6,65 @@
  */
 int main(int argc, char **argv, char **envp)
 {
-    char *line = NULL;
-    ssize_t nread;
-    size_t len = 0;
-    pid_t pid;
-    char *args[MAX_ARGS];
-    char *executable_path, *env;
-    char *path[MAX_ARGS];
-    char *path_var, *exit_command;
-    int i;
+    char *input_line = NULL;
+    ssize_t input_size;
+    size_t line_length = 0;
+    pid_t child_pid;
+    char *arguments[MAX_ARGS];
+    char *executable_path, *environment;
+    char *search_paths[MAX_ARGS];
+    char *path_variable, *exit_command;
+    int path_index;
 
     (void)argc;
     (void)argv;
     exit_command = "exit";
-    env = "env";
-    path_var = getenv("PATH");
-    if (path_var == NULL)
+    environment = "env";
+    path_variable = getenv("PATH");
+    if (path_variable == NULL)
     {
         perror("./hsh");
         return (EXIT_FAILURE);
     }
 
-    i = 0;
-    path[i++] = strtok(path_var, ":");
-    while ((path[i] = strtok(NULL, ":")) != NULL)
+    path_index = 0;
+    search_paths[path_index++] = strtok(path_variable, ":");
+    while ((search_paths[path_index] = strtok(NULL, ":")) != NULL)
     {
-        i++;
+        path_index++;
     }
 
     while (1)
     {
         _print_str("");
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
+        input_size = getline(&input_line, &line_length, stdin);
+        if (input_size == -1)
             break;
 
-        remove_newline(line);
-        tokenize_args(args, line);
-        if (args[0] == NULL)
+        remove_newline(input_line);
+        tokenize_args(arguments, input_line);
+        if (arguments[0] == NULL)
             continue;
-        
-        if (strcmp(args[0], exit_command) == 0)
+
+        if (strcmp(arguments[0], exit_command) == 0)
             break;
-        if (strcmp(args[0], env) == 0)
+        if (strcmp(arguments[0], environment) == 0)
             print_env(envp);
 
-        executable_path = find_the_path(args[0], path);
+        executable_path = find_the_path(arguments[0], search_paths);
         if (executable_path != NULL)
         {
-            pid = fork();
+            child_pid = fork();
 
-            if (pid < 0)
+            if (child_pid < 0)
             {
                 perror("Process execution failed");
                 continue;
             }
 
-            else if (pid == 0)
+            else if (child_pid == 0)
             {
-                execve(executable_path, args, envp);
+                execve(executable_path, arguments, envp);
                 perror("./shell");
                 _exit(EXIT_FAILURE);
             }
@@ -72,6 +72,6 @@ int main(int argc, char **argv, char **envp)
         }
     }
 
-    free(line);
+    free(input_line);
     return (0);
 }
